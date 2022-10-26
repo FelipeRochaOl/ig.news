988153d3c58518c4d845d5b974ca921c6ec50e61
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-10-26 00:41:24 -0300

    Add component SubscribeButton

--- src/components/Header/SubscriveButton/index.tsx
+++ src/components/Header/SubscriveButton/index.tsx
@@ -1,7 +1,8 @@
 
+import { ISubscriptionButtonProps } from './interfaces';
 import styles from './styles.module.scss';
 
-export function SubscribeButton() {
+export function SubscribeButton({priceId}: ISubscriptionButtonProps) {
   return (
   <button type="button" className={styles.subscribeButton}>
     Subscribe now
