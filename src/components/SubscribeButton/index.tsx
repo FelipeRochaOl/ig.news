b2ad31141cca20271dd8928636fa39aa8eb2c91c
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-10-30 02:12:47 -0300

    Add SubscribeButton component

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,40 @@
+
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { api } from '../../services/api';
+import { getStripeJS } from '../../services/stripe-js';
+import { ISubscriptionButtonProps } from './interfaces';
+import styles from './styles.module.scss';
+
+export function SubscribeButton({ priceId, text }: ISubscriptionButtonProps) {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts')
+      return;
+    }
+
+    try {
+     const response = await api.post('/subscribe')
+     const {sessionId} = response.data;
+     const stripeJS = await getStripeJS();
+
+     await stripeJS?.redirectToCheckout({sessionId});
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+  <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
+    {text}
+  </button>
+  )
+}
--- src/components/SubscribeButton/interfaces.ts
+++ src/components/SubscribeButton/interfaces.ts
@@ -1,3 +1,4 @@
 export interface ISubscriptionButtonProps {
+  text: string;
   priceId: string;
 }
